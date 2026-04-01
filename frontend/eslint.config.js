a37fcd57ba62ed8526293bbbf27868277935d3ea
SafouaneHaddadi <safouane.haddadi@condorcet.be>
2026-04-01 16:24:47 +0200

    ci: fix eslint jsx parsing for frontend

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,17 +1,37 @@
 import js from "@eslint/js";
+import reactPlugin from "eslint-plugin-react";
 
 export default [
   js.configs.recommended,
   {
     files: ["src/**/*.js", "src/**/*.jsx"],
-    rules: {
-      "no-unused-vars": "warn",
-      "no-console": "off",
-      "react/prop-types": "off",
+    plugins: {
+      react: reactPlugin,
     },
     languageOptions: {
       ecmaVersion: 2022,
       sourceType: "module",
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        console: "readonly",
+        document: "readonly",
+        window: "readonly",
+      },
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    rules: {
+      "no-unused-vars": "warn",
+      "no-console": "off",
+      "react/prop-types": "off",
+      "react/react-in-jsx-scope": "off",
     },
   },
 ];
