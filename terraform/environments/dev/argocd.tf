38c3d453bdeab637673f897dd99faf46787f609e
SafouaneHaddadi <safouane.haddadi@condorcet.be>
2026-04-01 21:23:08 +0200

    feat: make LoadBalancer internet-facing via Helm annotation [skip ci]

--- terraform/environments/dev/argocd.tf
+++ terraform/environments/dev/argocd.tf
@@ -22,6 +22,11 @@ resource "helm_release" "argocd" {
     name  = "server.service.type"
     value = "LoadBalancer"
   }
+  
+  set {
+    name  = "server.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-scheme"
+    value = "internet-facing"
+  }
 
   # wait for the EKS cluster to be ready
   depends_on = [
